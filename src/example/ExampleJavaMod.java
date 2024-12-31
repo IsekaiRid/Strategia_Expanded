b508bf8bcae0958846fe22bf04355bc8754956b4
IsekaiRid <mainrpg62@gmail.com>
2024-12-31 17:00:18 +0700

    push_one

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -9,28 +9,29 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class ExampleJavaMod extends Mod {
 
-    public ExampleJavaMod(){
+    public ExampleJavaMod() {
         Log.info("Loaded ExampleJavaMod constructor.");
 
-        //listen for game load event
+        // Mendengarkan event saat game dimuat
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
+            // Menampilkan dialog setelah startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                BaseDialog dialog = new BaseDialog("Komando");
+                dialog.cont.add("Halo, saya Komando.").row();
+                dialog.cont.add("Garis pertahanan pertama bumi.").row();
+                dialog.cont.add("Salam kenal, mari kita bertahan dari ambang kehancuran.").row();
+                // Menampilkan gambar (ganti 'example-java-mod-icon' dengan sprite Anda)
+                dialog.cont.image(Core.atlas.find("example-java-mod-icon")).pad(20f).row();
+                dialog.cont.button("Oke", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
     }
 
     @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
+    public void loadContent() {
+        Log.info("Loading Strategia Expanded content.");
     }
-
 }
