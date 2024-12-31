d1e8c21f05c77e2e5d6daca7cacf603e7778f9e5
IsekaiRid <mainrpg62@gmail.com>
2025-01-01 01:19:59 +0700

    updateBug3

--- src/utility/CustomDialog.java
+++ src/utility/CustomDialog.java
@@ -4,9 +4,10 @@ import arc.scene.ui.Dialog;
 import arc.scene.ui.Label;
 import arc.scene.ui.TextButton;
 import arc.scene.ui.layout.Table;
-import arc.scene.style.TextureRegionDrawable;
-import arc.graphics.g2d.TextureRegion;
-import arc.util.Time;
+import arc.scene.style.LabelStyle;
+import arc.graphics.g2d.BitmapFont;
+import arc.graphics.g2d.Font;
+import arc.Core;
 
 public class CustomDialog extends Dialog {
 
@@ -20,8 +21,15 @@ public class CustomDialog extends Dialog {
         this.setMovable(true); // Membuat dialog bisa digeser
         this.setModal(true);   // Membuat dialog sebagai modal
 
+        // Membuat font dari default font yang ada
+        BitmapFont font = Core.font;  // Menggunakan font default dari Core
+
+        // Membuat LabelStyle dengan font yang sudah ditentukan
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.font = font; // Set font untuk label
+
         // Menambahkan konten ke dialog
-        cont.add(new Label("This is a custom dialog!")).row(); // Tambahkan label
+        cont.add(new Label("This is a custom dialog!", labelStyle)).row(); // Tambahkan label dengan gaya
         cont.add(new TextButton("Click Me", new TextButton.TextButtonStyle())).row(); // Tambahkan tombol
 
         // Menambahkan tombol ke bagian bawah dialog
@@ -39,8 +47,6 @@ public class CustomDialog extends Dialog {
     @Override
     public Dialog show() {
         super.show();
-        // Set the dialog to appear for a specified time (example: 5 seconds)
-        Time.runTask(5f, this::hide);
         return this;
     }
 }
