13805d3e775418855e6cf43ff41a6306610a6594
IsekaiRid <mainrpg62@gmail.com>
2025-01-01 17:35:17 +0700

    restart5

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -9,14 +9,8 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 import mindustry.ui.Styles;
-import mindustry.ui.Icon;
-import mindustry.graphics.Pal;
-import arc.graphics.Color;
 
 public class ExampleJavaMod extends Mod {
-
-    private static Links.LinkEntry[] links;
-    private static final String MOD_GITHUB_URL = "https://github.com/Yuria-Shikibe/NewHorizonMod";
 
     public ExampleJavaMod() {
         Log.info("Loaded ExampleJavaMod constructor.");
@@ -25,40 +19,23 @@ public class ExampleJavaMod extends Mod {
         Events.on(ClientLoadEvent.class, e -> {
             // Menampilkan dialog setelah startup
             Time.runTask(10f, () -> {
+                // Menampilkan dialog pertama menggunakan BaseDialog
                 BaseDialog dialog = new BaseDialog("anime");
                 dialog.cont.add("Halo, saya Komando.").row();
                 dialog.cont.add("Garis pertahanan pertama bumi.").row();
                 dialog.cont.add("Salam kenal, mari kita bertahan dari ambang kehancuran.").row();
 
+                // Menambahkan tombol dengan tautan ke repository
+                dialog.cont.button("Repo saya", () -> Core.app.openURI("https://github.com/IsekaiRid/Strategia_Expanded")).size(200f, 50f).row();
+
                 // Menampilkan gambar (gunakan sprite yang ada atau sesuaikan)
                 dialog.cont.image(Core.atlas.find("anime")).pad(20f).row();
+
+                // Menambahkan tombol untuk menutup dialog
                 dialog.cont.button("Oke", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
         });
-    }
-
-    // Menampilkan dialog tentang mod
-    private static void showAbout() {
-        if (links == null) {
-            links = new Links.LinkEntry[]{
-                new Links.LinkEntry("mod.ccs", "https://github.com/Yuria-Shikibe/NewHorizonMod/wiki/Cutscene-Script-Custom-Guide", Icon.settings, Pal.heal),
-                new Links.LinkEntry("mod.discord", "https://discord.gg/yNmbMcuwyW", Icon.discord, Color.valueOf("7289da")),
-                new Links.LinkEntry("mod.github", MOD_GITHUB_URL, Icon.github, Color.valueOf("24292e")),
-                new Links.LinkEntry("mod.guide", "https://github.com/Yuria-Shikibe/NewHorizonMod#mod-guide", Icon.bookOpen, Pal.accent),
-                new Links.LinkEntry("yuria.plugin", "https://github.com/Yuria-Shikibe/RangeImager", Icon.export, Color.valueOf("ff4c4c"))
-            };
-        }
-
-        BaseDialog dialog = new BaseDialog("@links");
-        dialog.cont.pane(table -> {
-            for (Links.LinkEntry entry : links) {
-                TableFunc.link(table, entry);
-            }
-        }).grow().row();
-        dialog.cont.button("@back", Icon.left, Styles.cleart, dialog::hide).size(150f, 50f);
-        dialog.addCloseListener();
-        dialog.show();
     }
 
     @Override
